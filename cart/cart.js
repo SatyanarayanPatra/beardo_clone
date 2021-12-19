2ba71027d68960761a6d8a4192f9a7eff698cc90
Kanhaiya <kanhaiya15feb@gmail.com>
2021-12-19 21:56:59 +0530

    all Updated code

---
+++ cart/cart.js
@@ -0,0 +1,34 @@
+function cartDiv() {
+  return `  <div class="detailContainer">
+    <div class="left">
+      <img src="" alt="" />
+    </div>
+    <div class="right">
+      <div class="nameDiv">
+        <h2></h2>
+      </div>
+      <div class="priceDiv">
+        <p>585255</p>
+        <p style="text-decoration: line-through red">52522</p>
+        <p>25855</p>
+        <p>% off</p>
+      </div>
+      <div class="qtyDiv">
+        <p>Qty</p>
+        <div class="qtyDiv">
+          <select name="" id="">
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+            <option value="5"></option>
+          </select>
+        </div>
+        <dir>REMOVE</dir>
+      </div>
+    </div>
+  </div>
+</div>`
+}
+
+export default cartDiv;
---
+++ component/allDiv.js
@@ -0,0 +1,24 @@
+function allDiv() {
+
+  return `<div id="container">
+    <div id="row1">
+    <h2 id="getCount" style="color: red; font-weight: bolder"></h2>
+      <div id="divRight">
+        <select name="" id="selectOrder">
+          <option value="select" style="font-weight: lighter">
+            RCOMENDED
+          </option>
+          <option value="low to high" style="font-weight: lighter">
+            LOW TO HIGH
+          </option>
+          <option value="high to low" style="font-weight: lighter">
+            HIGH TO LOW
+          </option>
+        </select>
+      </div>
+    </div>
+    <div id="row2"></div>
+  </div>`
+}
+
+export default allDiv;
